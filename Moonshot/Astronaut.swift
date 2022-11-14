f8b0b2620c635b6d31b46eb189e919dbde00bad5
CodeByDzyne <wdenley@dzynesolutions.com>
2022-11-14 12:42:55 +1100

    Mission view complete

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Wealthy Waz on 7/11/2022.
+//
+
+import Foundation
+
+
+struct Astronaut: Identifiable, Codable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ Moonshot/Mission.swift
@@ -0,0 +1,32 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Wealthy Waz on 7/11/2022.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+
+    let id: Int
+    let launchDate: Date?
+    let crew: [CrewRole]
+    let description: String
+    
+    var displayName: String {
+        "Apollo \(id)"
+    }
+    
+    var imageName: String {
+        "apollo\(id)"
+    }
+    
+    var formattedLaunchDate: String {
+        launchDate?.formatted(date: .abbreviated, time: .omitted) ?? "N/A"
+    }
+}
